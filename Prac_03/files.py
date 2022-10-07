874d27a1297a53822c7bb7f893721cd52fcc2a3f
jckkl <jackkelly01@my.jcu.edu.au>
2022-10-07 22:03:39 +1000

    Finished final part of files.py

--- Prac_03/files.py
+++ Prac_03/files.py
@@ -18,5 +18,34 @@ def name_reader():
     in_file.close()
 
 
-
 name_reader()
+
+# 3.
+"""add 1st number to second"""
+
+
+def number_adder():
+    with open("numbers.txt", "r") as in_file:
+        text = in_file.readlines()
+        solution = int(text[0]) + int(text[1])
+        in_file.close()
+        print(solution)
+
+
+number_adder()
+
+
+# 4
+def all_numbers_adder():
+    with open("numbers.txt", "r") as in_file:
+        text = in_file.readlines()
+        i = 0
+        sum = 0
+        for i in range(0, len(text)):
+            sum = sum + int(text[i])
+            i = i + 1
+        in_file.close()
+        print(sum)
+
+
+all_numbers_adder()
