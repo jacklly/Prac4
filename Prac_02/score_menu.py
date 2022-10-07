2bcc659504c24642af9aad51a1609ce7235dd669
jckkl <jackkelly01@my.jcu.edu.au>
2022-10-07 14:15:33 +1000

    Created and finished score_menu.py

---
+++ Prac_02/score_menu.py
@@ -0,0 +1,52 @@
+import random
+
+score = -1
+
+
+def choiceengine():
+    choice = str(input(" (G)et a valid score\n (P)rint result\n (S)how stars\n (Q)uit").upper())
+    return (choice)
+
+
+def score_returner(score):
+    if score > 100 or score < 0:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent score.")
+    elif score >= 50:
+        print("Passable score")
+    else:
+        print("Bad score")
+
+
+def random_scorer2():
+    score = random.randint(0, 100)
+    return score
+
+
+def main():
+    choice = choiceengine()
+    while choice != 'Q':
+        if choice == 'G':
+            score = random_scorer2()
+            print("Your score is: {}".format(score))
+            choice = choiceengine()
+        elif choice == 'P':
+            if score == -1:
+                print("Please get a valid score first!")
+                choice = choiceengine()
+            else:
+                print("Your score is: {}".format(score))
+                score_returner(score)
+                choice = choiceengine()
+        elif choice == 'S':
+            if score == -1:
+                print("Please get a valid score first!")
+                choice = choiceengine()
+            else:
+                print(score * "*")
+                choice = choiceengine()
+
+main()
+
+print("Thank you for using this function!")
