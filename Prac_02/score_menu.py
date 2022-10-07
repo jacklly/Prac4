b53371b7d9f5920f296fcd9d4913f69fe2c6780f
jckkl <jackkelly01@my.jcu.edu.au>
2022-10-07 14:22:54 +1000

    Negligible changes made to score_menu.py

--- Prac_02/score_menu.py
+++ Prac_02/score_menu.py
@@ -1,22 +1,20 @@
 import random
 
-score = -1
-
 
 def choiceengine():
-    choice = str(input(" (G)et a valid score\n (P)rint result\n (S)how stars\n (Q)uit").upper())
+    choice = str(input(" (G)et a valid score\n (P)rint result\n (S)how stars\n (Q)uit\n ").upper())
     return (choice)
 
 
 def score_returner(score):
     if score > 100 or score < 0:
-        print("Invalid score")
+        print("Invalid score\n ")
     elif score >= 90:
-        print("Excellent score.")
+        print("Excellent score\n ")
     elif score >= 50:
-        print("Passable score")
+        print("Passable score\n ")
     else:
-        print("Bad score")
+        print("Bad score\n ")
 
 
 def random_scorer2():
@@ -25,27 +23,29 @@ def random_scorer2():
 
 
 def main():
+    score = -1
     choice = choiceengine()
     while choice != 'Q':
         if choice == 'G':
             score = random_scorer2()
-            print("Your score is: {}".format(score))
+            print("Your score is: {}\n ".format(score))
             choice = choiceengine()
         elif choice == 'P':
             if score == -1:
-                print("Please get a valid score first!")
+                print("Please get a valid score first!\n ")
                 choice = choiceengine()
             else:
-                print("Your score is: {}".format(score))
+                print("Your score is: {}\n ".format(score))
                 score_returner(score)
                 choice = choiceengine()
         elif choice == 'S':
             if score == -1:
-                print("Please get a valid score first!")
+                print("Please get a valid score first!\n ")
                 choice = choiceengine()
             else:
                 print(score * "*")
                 choice = choiceengine()
+
 
 main()
 
