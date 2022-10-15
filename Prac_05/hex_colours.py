392610845f6af2e01f34224595270b985a596db4
jckkl <jackkelly01@my.jcu.edu.au>
2022-10-15 17:21:58 +1000

    Completed hex_colours.py

---
+++ Prac_05/hex_colours.py
@@ -0,0 +1,27 @@
+colours = {"absolutezero": "#0048ba", "chestnut": "#954535", "darklavendar": "#734f96", "ferrarired": "#ff2800",
+           "jazzberryjam": "#a50b5e", "pinkflamingo": "#fc74fd", "vanillaice": "#f38fa9", "zomp": "#39a78e",
+           "egyptianblue": "#1034a6", "byzantine": "#bd33a4"}
+
+open_message = "Colour choices are:"
+
+print(open_message)
+number = 1
+for colour in colours:
+    print(f"{number:3}. {colour}")
+    number = number + 1
+
+
+def colour_displayer():
+    colour_choice = input("Please choose a colour name to reveal its code, or leave blank to exit.\nEntry: ").lower()
+    while colour_choice != "":
+        try:
+            print(f"{colour_choice} has the colour code: {colours[colour_choice]}")
+        except ValueError:
+            print(f"Please enter a valid colour name!")
+        colour_choice = input("Please choose a colour, or leave blank to exit.\nEntry: ").lower()
+    if colour_choice == "":
+        print("Thank you!")
+        quit()
+
+
+colour_displayer()
