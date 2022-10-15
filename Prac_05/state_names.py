5af7e72ceca572e2271bbb3f136b7b55fca86edf
jckkl <jackkelly01@my.jcu.edu.au>
2022-10-15 16:31:04 +1000

    Completed state_names.py

--- Prac_05/state_names.py
+++ Prac_05/state_names.py
@@ -4,15 +4,20 @@ State names in a dictionary
 File needs reformatting
 """
 
-# DOING: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT" : "Northern Territory", "WA" : "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+# DONE: Reformat this file so the dictionary code follows PEP 8 convention
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 
-state_code = input("Enter short state: ")
+max_length_short = max(len(pair) for pair in CODE_TO_NAME)
+max_length_long = max(len(CODE_TO_NAME[pair]) for pair in CODE_TO_NAME)
+
+for pair in CODE_TO_NAME:
+    print(f"{pair:{max_length_short}} is {CODE_TO_NAME[pair]:{max_length_long}}")
+
+state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    try:
+        print(f"{state_code:{max_length_short}} is {CODE_TO_NAME[state_code]:{max_length_long}}")
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
