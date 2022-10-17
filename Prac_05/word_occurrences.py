00d3214ce6ffccdeaf2aa7ca952d0364be55c52e
jckkl <jackkelly01@my.jcu.edu.au>
2022-10-17 13:18:14 +1000

    Completed word_occurrences.py

--- Prac_05/word_occurrences.py
+++ Prac_05/word_occurrences.py
@@ -8,14 +8,15 @@ words_dictionary = {}
 text = input("Please input your text: ").lower()
 
 words = text.split()
+"""this means a sorting program is not required, making the code more efficient"""
 words.sort()
+longest_word = max((len(word) for word in words))
 
 for word in words:
     if word not in words_dictionary:
         words_dictionary[word] = 0
     words_dictionary[word] += 1
 
-for set in words_dictionary:
-    print(f"{set} occurs {words_dictionary[set]} times")
 
-print(words_dictionary)
+for set in words_dictionary:
+    print(f"{set:{longest_word}}: {words_dictionary[set]}")
