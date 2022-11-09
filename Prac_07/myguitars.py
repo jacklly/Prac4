2c1e8e5574c1e3f25c1b4c13a15a973ddb26e0e6
jacklly <jack.kelly1@my.jcu.edu.au>
2022-11-09 20:02:08 +1000

    Initial commit

---
+++ Prac_07/myguitars.py
@@ -0,0 +1,21 @@
+"""
+Guitar Program!
+Started: 6 30
+Finished:
+"""
+
+from Prac_06.guitar import Guitar
+
+
+def main():
+    with open("guitars.csv", "r", encoding="utf-8-sig") as in_file:
+        guitars = []
+        for line in in_file:
+            split_line = line.strip().split(",")
+            guitar = Guitar(split_line[0], split_line[1], float(split_line[2]))
+            guitars.append(guitar)
+    for guitar in guitars:
+        print(guitar)
+    guitars.sort()
+
+main()
