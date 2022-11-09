f22c67056b43ae0a9b4b98c5c9e68aa196c1c5ec
jckkl <jackkelly01@my.jcu.edu.au>
2022-11-10 09:33:52 +1000

    refactored into functions

--- Prac_07/myguitars.py
+++ Prac_07/myguitars.py
@@ -9,16 +9,30 @@ from Prac_06.guitar import Guitar
 
 def main():
     """create guitar lists"""
-    with open("guitars.csv", "r", encoding="utf-8-sig") as in_file:
-        guitars = []
-        savable_guitars = []
-        for line in in_file:
-            split_line = line.strip().split(",")
-            savable_line = line.strip()
-            guitar = Guitar(split_line[0], split_line[1], float(split_line[2]))
-            guitars.append(guitar)
-            savable_guitars.append(savable_line)
+    guitars, savable_guitars = list_creation()
     """add guitars"""
+    guitar_adder(guitars, savable_guitars)
+    """sort guitars"""
+    guitars.sort()
+    """display guitars"""
+    display_guitars(guitars)
+    """save guitars"""
+    save_guitars(savable_guitars)
+
+
+def save_guitars(savable_guitars):
+    with open("guitars.csv", "w", encoding="utf-8-sig") as out_file:
+        for guitar in savable_guitars:
+            print(guitar, file=out_file)
+
+
+def display_guitars(guitars):
+    print("Your guitars:")
+    for guitar in guitars:
+        print(guitar)
+
+
+def guitar_adder(guitars, savable_guitars):
     choice = input("Do you wish to add a guitar? (Y/N)").upper()
     while choice == "Y":
         name = input("Name: ")
@@ -29,18 +43,19 @@ def main():
         savable_guitars.append(additive)
         guitars.append(Guitar(name, year, float(cost)))
         choice = input("Do you wish to add another guitar? (Y/N)").upper()
-    """sort guitars"""
-    print(guitars)
-    guitars.sort()
-    """display guitars"""
-    print("Your guitars:")
-    for guitar in guitars:
-        print(guitar)
-    """save guitars"""
-    with open("guitars.csv", "w", encoding="utf-8-sig") as out_file:
-        for guitar in savable_guitars:
-            print(guitar, file=out_file)
+
+
+def list_creation():
+    with open("guitars.csv", "r", encoding="utf-8-sig") as in_file:
+        guitars = []
+        savable_guitars = []
+        for line in in_file:
+            split_line = line.strip().split(",")
+            savable_line = line.strip()
+            guitar = Guitar(split_line[0], split_line[1], float(split_line[2]))
+            guitars.append(guitar)
+            savable_guitars.append(savable_line)
+    return guitars, savable_guitars
 
 
 main()
-
