a80aebc7eabd529cdee2901bf66e0d08a305927f
jacklly <jack.kelly1@my.jcu.edu.au>
2022-11-23 13:41:21 +1000

    Complete first half of taxi test

---
+++ Prac_09/taxi_test.py
@@ -0,0 +1,18 @@
+"""
+taxi test for prac 9
+Jack Kelly
+"""
+
+from taxi import Taxi
+
+def main():
+    """taxi test"""
+    my_taxi = Taxi(100, "Prius 1", 1.23)
+    my_taxi.get_fare()
+    my_taxi.drive(40)
+    print(my_taxi)
+    print(my_taxi.get_fare())
+
+
+
+main()
