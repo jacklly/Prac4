5886aaa5b0d340d45dce0dac352bc24360253620
jacklly <jack.kelly1@my.jcu.edu.au>
2022-11-23 13:44:27 +1000

    Complete set price exercise

--- Prac_09/taxi.py
+++ Prac_09/taxi.py
@@ -8,8 +8,9 @@ from Prac_09.car import Car
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
-    def __init__(self, name, fuel, price_per_km):
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
+        price_per_km = 1.23
         super().__init__(name, fuel)
         self.price_per_km = price_per_km
         self.current_fare_distance = 0
--- Prac_09/taxi_test.py
+++ Prac_09/taxi_test.py
@@ -7,7 +7,7 @@ from taxi import Taxi
 
 def main():
     """taxi test"""
-    my_taxi = Taxi(100, "Prius 1", 1.23)
+    my_taxi = Taxi(100, "Prius 1")
     my_taxi.get_fare()
     my_taxi.drive(40)
     print(my_taxi)
