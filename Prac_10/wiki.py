bd1018256c107520425969ece9c6bf007f8d42e1
jacklly <jack.kelly1@my.jcu.edu.au>
2022-11-30 18:31:08 +1000

    Complete wiki program

---
+++ Prac_10/wiki.py
@@ -0,0 +1,9 @@
+import wikipedia
+
+user_input = None
+while user_input != "":
+    user_input = input("Search Wikipedia:\n>>>")
+    page = wikipedia.page(f"{user_input}")
+    print(f"{page.title} \n{page.url}")
+    print(wikipedia.summary(f"{user_input}"))
+    print(" ")
